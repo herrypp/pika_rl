9f5310b5d96c44593041e3068b53474f17be0b64
herrypp <herrywang525@gmail.com>
2018-10-24 17:02:10 +0800

    take position of objects as input

--- state.py
+++ state.py
@@ -15,10 +15,12 @@ class State:
         self.is_episode_start = False
         self.is_step_start = False
         self.is_score_change = False
+        self.input = None
 
     def update(self):
         self.update_screen()
         self.update_score()
+        self.update_input()
 
     def update_screen(self):
         # screenshot the pikaball window
@@ -27,10 +29,13 @@ class State:
         np_image = np.asarray(pil_image)
         # We just concern about pikachu & ball
         filter_image = self.img_filtering(np_image)
+        # cv2.imshow('resize image', cv2.resize(filter_image, (enviroment.input_height, enviroment.input_width)))
+        # cv2.waitKey(1)
         cv2_image = cv2.cvtColor(filter_image, cv2.COLOR_RGB2GRAY)
 
         self.screen = cv2_image
-        self.resize_screen = cv2.resize(cv2_image, (enviroment.input_height, enviroment.input_width))
+        self.resize_screen = cv2.resize(cv2_image, (enviroment.input_width, enviroment.input_height))
+        
 
     def update_score(self):
         self.is_score_change = False
@@ -102,3 +107,71 @@ class State:
             return 5
         else:
             return -1
+
+    def update_input(self):
+        object_list = []
+        object_list.extend(self.find_object(Object.Left_pika))
+        object_list.extend(self.find_object(Object.Right_pika))
+        object_list.extend(self.find_object(Object.Ball))
+        
+        self.input = np.asarray(object_list)
+            
+        print(self.input)
+        
+        
+
+    def find_object(self, obj):
+        start = 0
+        end = 0
+        light = 0
+        size_x = 0
+        size_y = 0
+        if obj == Object.Left_pika:
+            start = 0
+            end = int(enviroment.input_width/2)
+            light = 232
+            size_y = 1
+            size_x = 12
+        elif obj == Object.Right_pika:
+            start = int(enviroment.input_width/2)
+            end = enviroment.input_width
+            light = 232
+            size_y = 1
+            size_x = 12
+        else:
+            start = 0
+            end = enviroment.input_width
+            light = 113
+            size_y = 5
+            size_x = 5
+
+
+        method = cv2.TM_SQDIFF_NORMED
+
+        # find pika left
+        obj_pattern = light*np.ones([size_y, size_x]).astype(np.uint8)
+        image = self.resize_screen[:,start:end]
+
+        print(obj_pattern.shape)
+        print(image.shape)
+
+        result = cv2.matchTemplate(obj_pattern, image, method)
+        position = np.where(result < 0.1)
+        pos_num = len(position[0])
+        if pos_num == 0:
+            return [-1, -1]
+        x = sum(position[1])/pos_num
+        y = sum(position[0])/pos_num
+        if obj == Object.Left_pika:
+            x = x + 8
+            y = y + 4
+        elif obj == Object.Right_pika:
+            x = x + int(enviroment.input_width/2) + 8
+            y = y + 4
+
+        return [int(x), int(y)]
+
+class Object:
+    Left_pika = 0
+    Right_pika = 1
+    Ball = 2
